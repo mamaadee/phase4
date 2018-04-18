6191496eb75d80aa6e6aadcd1ecfa05f30882c3f
Maryam <mamaadee@andrew.cmu.edu>
2018-04-18 19:48:38 +0000

    Added students contexts

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should belong_to(:family)
+  should have_many(:registrations)
+  should have_many(:camps).through(:registrations)
+  
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
+  should validate_presence_of(:family_id)
+  
+  should allow_value(12.years.ago.to_date).for(:date_of_birth)
+  should allow_value(7.years.ago.to_date).for(:date_of_birth)
+  
+  should_not allow_value(Date.today).for(:date_of_birth)
+  should_not allow_value(1.day.from_now.to_date).for(:date_of_birth)
+
 end
--- test/sets/student_contexts.rb
+++ test/sets/student_contexts.rb
@@ -1,5 +1,23 @@
 module Contexts
     module StudentContexts
-    
+        def create_students
+            @maryam    = FactoryBot.create(:student, family: @almaadeed, rating: 100)
+            @amna   = FactoryBot.create(:student, family: @alhaj, first_name: "Amna", last_name: "Alhaj", date_of_birth: 11.years.ago.to_date, rating: 1929)
+            @dan    = FactoryBot.create(:student, family: @phelps, first_name: "Daniel", last_name: "Phelps", date_of_birth: 18.years.ago.to_date, rating: 678) 
+        end
+        
+        def delete_students
+            @maryam.delete
+            @amna.delete
+            @dan.delete
+        end
+        
+        def create_inactive_students
+            @fatma = FactoryBot.create(:student, family: @alsheeb, first_name: "Fatma", last_name: "AlSheeb", date_of_birth: 5.years.ago.to_date, active: false, rating: nil)
+        end
+
+        def delete_inactive_students
+            @fatma
+        end
     end
 end

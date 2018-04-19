3870e1012e0c2f3961ff443ab93d000373094018
Maryam <mamaadee@andrew.cmu.edu>
2018-04-19 08:45:16 +0000

    Working on all the tests

--- test/models/camp_instructor_test.rb
+++ test/models/camp_instructor_test.rb
@@ -1,17 +1,17 @@
 require 'test_helper'
 
 class CampInstructorTest < ActiveSupport::TestCase
-  # test relationships
+  #relationships
   should belong_to(:camp)
   should belong_to(:instructor)
 
-  # test validations
+  #validations
   should validate_presence_of(:camp_id)
   should validate_presence_of(:instructor_id)
   should validate_numericality_of(:camp_id).only_integer.is_greater_than(0)
   should validate_numericality_of(:instructor_id).only_integer.is_greater_than(0)
 
-  # set up context
+  #context
   include Contexts
   context "Within context" do
     setup do 
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -14,5 +14,68 @@ class StudentTest < ActiveSupport::TestCase
   
   should_not allow_value(Date.today).for(:date_of_birth)
   should_not allow_value(1.day.from_now.to_date).for(:date_of_birth)
+  
+  context "context" do
+    setup do
+      create_family_users
+      create_families
+      create_students
+    end
+    
+    teardown do
+      delete_students
+      delete_families
+      delete_family_users
+    end
+    
+    should "sort students in alphabetical order" do
+      assert_equal ["Alhaj, Amna", "AlMaadeed, Maryam", "Alsheeb, Fatma", "Phelps, Daniel"], Student.alphabetical.all.map(&:name)
+    end
+    
+    should "have working age method" do 
+      assert_equal 11, @amna.age
+      assert_equal 18, @dan.age 
+    end
+    
+    should "show that name method works" do
+      assert_equal "Alhaj, Amna", @amna.name
+      assert_equal "Phelps, Daniel", @dan.name
+    end
+    
+    should "show that proper_name method works" do
+      assert_equal "Maryam Al-Maadeed", @maryam.proper_name
+      assert_equal "Fatma AlSheeb", @fatma.proper_name
+    end
+    
+    should "verify that student with no rating has default set to zero" do
+      create_inactive_students
+      assert_equal 0, @fatma.rating
+      delete_inactive_students
+    end
+    
+    should "verify that the student's family is active in the system" do
+      create_inactive_families
+      goerge = FactoryBot.build(:student, family: @phelps, first_name: "Goerge")
+      deny goerge.valid?
+      delete_inactive_families
+      alemadi = FactoryBot.build(:family, family_name: "Alemadi")
+      rouda = FactoryBot.build(:student, family: alemadi, first_name: "Rouda")
+      deny rouda.valid?
+    end
+    
+    should "show that there are six active students" do
+      create_inactive_students
+      assert_equal 3, Student.active.size
+      assert_equal ["Maryam", "Amna", "Daniel"], Student.active.all.map(&:first_name).sort
+      delete_inactive_students
+    end
+    
+    should "show that there is one inactive student" do
+      create_inactive_students
+      assert_equal 1, Student.inactive.size
+      assert_equal ["Fatma"], Student.inactive.all.map(&:first_name).sort
+      delete_inactive_students
+    end
+  end
 
 end
